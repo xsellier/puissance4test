5dfc10d0c8e76e0c9aad1f7af9d70e637885bfc5
binogure <binogure@c935e6ca-b7f9-11dd-b612-3927438af0ba>
2009-01-23 18:23:54 +0000

    ajout d'un ordi pas si bete que ca

--- src/GUI.java
+++ src/GUI.java
@@ -5,255 +5,254 @@
 
 package src;
 
-import java.awt.event.WindowAdapter;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import javax.swing.JFrame;
 
-public class GUI extends JFrame{
-    
-    //Variables graphiques
-    private javax.swing.JButton NewBut;
-    private javax.swing.JButton close;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JToggleButton jToggleButton1;
-    private javax.swing.JLabel outPut;
-    private javax.swing.JButton play;
-    private javax.swing.JButton play2;
-    private javax.swing.JButton play3;
-    private javax.swing.JButton play4;
-    private javax.swing.JButton play5;
-    private javax.swing.JButton play6;
-    private javax.swing.JButton play7;
-    private javax.swing.JPanel playGroundPan;
-    private javax.swing.JPanel playZone;
-    private javax.swing.JLabel title;
-    
-    public int choice;
-    public boolean played;
+@SuppressWarnings("serial")
+public class GUI extends JFrame {
 
-    public GUI(){
-	played = false;
-    }
+	// Variables graphiques
+	private javax.swing.JButton NewBut;
+	private javax.swing.JButton close;
+	private javax.swing.JPanel jPanel1;
+	private javax.swing.JPanel jPanel2;
+	private javax.swing.JScrollPane jScrollPane1;
+	private javax.swing.JToggleButton jToggleButton1;
+	private javax.swing.JLabel outPut;
+	private javax.swing.JButton play;
+	private javax.swing.JButton play2;
+	private javax.swing.JButton play3;
+	private javax.swing.JButton play4;
+	private javax.swing.JButton play5;
+	private javax.swing.JButton play6;
+	private javax.swing.JButton play7;
+	private javax.swing.JPanel playGroundPan;
+	private javax.swing.JPanel playZone;
+	private javax.swing.JLabel title;
 
-    public void initGui(){
-	initComponents();
-	for(int i=0; i<42; i++) {
-	    javax.swing.JButton tmpPan = new javax.swing.JButton();
-	    tmpPan.setEnabled(false);
-	    playZone.add( tmpPan );
+	public int choice;
+	public boolean played;
+
+	public GUI() {
+		played = false;
 	}
-	outPut.setText("C'est parti !!!");
-    }
-    
-    private void initComponents() {
-	jToggleButton1 = new javax.swing.JToggleButton();
-	playGroundPan = new javax.swing.JPanel();
-	jPanel1 = new javax.swing.JPanel();
-	play = new javax.swing.JButton();
-	play2 = new javax.swing.JButton();
-	play3 = new javax.swing.JButton();
-	play4 = new javax.swing.JButton();
-	play5 = new javax.swing.JButton();
-	play6 = new javax.swing.JButton();
-	play7 = new javax.swing.JButton();
-	playZone = new javax.swing.JPanel();
-	jPanel2 = new javax.swing.JPanel();
-	NewBut = new javax.swing.JButton();
-	jScrollPane1 = new javax.swing.JScrollPane();
-	outPut = new javax.swing.JLabel();
-	close = new javax.swing.JButton();
-	title = new javax.swing.JLabel();
-	
-	jToggleButton1.setText("jToggleButton1");
-	
-	
-	addWindowListener(new java.awt.event.WindowAdapter() {
-		public void windowClosing(java.awt.event.WindowEvent evt) {
-		    exitForm(evt);
+
+	public void initGui() {
+		initComponents();
+		for (int i = 0; i < 42; i++) {
+			javax.swing.JButton tmpPan = new javax.swing.JButton();
+			tmpPan.setEnabled(false);
+			playZone.add(tmpPan);
 		}
-     });
-	
-     playGroundPan.setLayout(new java.awt.BorderLayout());
+		outPut.setText("C'est parti !!!");
+	}
 
-     jPanel1.setLayout(new java.awt.GridLayout(1, 7));
+	private void initComponents() {
+		jToggleButton1 = new javax.swing.JToggleButton();
+		playGroundPan = new javax.swing.JPanel();
+		jPanel1 = new javax.swing.JPanel();
+		play = new javax.swing.JButton();
+		play2 = new javax.swing.JButton();
+		play3 = new javax.swing.JButton();
+		play4 = new javax.swing.JButton();
+		play5 = new javax.swing.JButton();
+		play6 = new javax.swing.JButton();
+		play7 = new javax.swing.JButton();
+		playZone = new javax.swing.JPanel();
+		jPanel2 = new javax.swing.JPanel();
+		NewBut = new javax.swing.JButton();
+		jScrollPane1 = new javax.swing.JScrollPane();
+		outPut = new javax.swing.JLabel();
+		close = new javax.swing.JButton();
+		title = new javax.swing.JLabel();
 
-     play.setText("1");
-     play.addActionListener(new java.awt.event.ActionListener() {
-     public void actionPerformed(java.awt.event.ActionEvent evt) {
-     playIActionPerformed(evt,0);
-     }
-     });
+		jToggleButton1.setText("jToggleButton1");
 
-     jPanel1.add(play);
+		addWindowListener(new java.awt.event.WindowAdapter() {
+			public void windowClosing(java.awt.event.WindowEvent evt) {
+				exitForm(evt);
+			}
+		});
 
-     play2.setText("2");
-     play2.addActionListener(new java.awt.event.ActionListener() {
-     public void actionPerformed(java.awt.event.ActionEvent evt) {
-     playIActionPerformed(evt,1);
-     }
-     });
+		playGroundPan.setLayout(new java.awt.BorderLayout());
 
-     jPanel1.add(play2);
+		jPanel1.setLayout(new java.awt.GridLayout(1, 7));
 
-     play3.setText("3");
-     play3.addActionListener(new java.awt.event.ActionListener() {
-     public void actionPerformed(java.awt.event.ActionEvent evt) {
-     playIActionPerformed(evt,2);
-     }
-     });
+		play.setText("1");
+		play.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				playIActionPerformed(evt, 0);
+			}
+		});
 
-     jPanel1.add(play3);
+		jPanel1.add(play);
 
-     play4.setText("4");
-     play4.addActionListener(new java.awt.event.ActionListener() {
-     public void actionPerformed(java.awt.event.ActionEvent evt) {
-     playIActionPerformed(evt,3);
-     }
-     });
+		play2.setText("2");
+		play2.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				playIActionPerformed(evt, 1);
+			}
+		});
 
-     jPanel1.add(play4);
+		jPanel1.add(play2);
 
-     play5.setText("5");
-     play5.addActionListener(new java.awt.event.ActionListener() {
-     public void actionPerformed(java.awt.event.ActionEvent evt) {
-     playIActionPerformed(evt,4);
-     }
-     });
+		play3.setText("3");
+		play3.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				playIActionPerformed(evt, 2);
+			}
+		});
 
-     jPanel1.add(play5);
+		jPanel1.add(play3);
 
-     play6.setText("6");
-     play6.addActionListener(new java.awt.event.ActionListener() {
-     public void actionPerformed(java.awt.event.ActionEvent evt) {
-     playIActionPerformed(evt,5);
-     }
-     });
+		play4.setText("4");
+		play4.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				playIActionPerformed(evt, 3);
+			}
+		});
 
-     jPanel1.add(play6);
+		jPanel1.add(play4);
 
-     play7.setText("7");
-     play7.addActionListener(new java.awt.event.ActionListener() {
-     public void actionPerformed(java.awt.event.ActionEvent evt) {
-     playIActionPerformed(evt,6);
-     }
-     });
+		play5.setText("5");
+		play5.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				playIActionPerformed(evt, 4);
+			}
+		});
 
-     jPanel1.add(play7);
+		jPanel1.add(play5);
 
-     playGroundPan.add(jPanel1, java.awt.BorderLayout.SOUTH);
+		play6.setText("6");
+		play6.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				playIActionPerformed(evt, 5);
+			}
+		});
 
-     playZone.setLayout(new java.awt.GridLayout(6, 7));
+		jPanel1.add(play6);
 
-     playZone.setBackground(new java.awt.Color(51, 51, 255));
-     playGroundPan.add(playZone, java.awt.BorderLayout.CENTER);
+		play7.setText("7");
+		play7.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				playIActionPerformed(evt, 6);
+			}
+		});
 
-     getContentPane().add(playGroundPan, java.awt.BorderLayout.CENTER);
+		jPanel1.add(play7);
 
-     jPanel2.setLayout(new java.awt.GridLayout(1, 3));
+		playGroundPan.add(jPanel1, java.awt.BorderLayout.SOUTH);
 
-     NewBut.setText("Nouveau");
-     NewBut.addActionListener(new java.awt.event.ActionListener() {
-     public void actionPerformed(java.awt.event.ActionEvent evt) {
-     NewButActionPerformed(evt);
-     }
-     });
+		playZone.setLayout(new java.awt.GridLayout(6, 7));
 
-     jPanel2.add(NewBut);
+		playZone.setBackground(new java.awt.Color(51, 51, 255));
+		playGroundPan.add(playZone, java.awt.BorderLayout.CENTER);
 
-     outPut.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-     jScrollPane1.setViewportView(outPut);
+		getContentPane().add(playGroundPan, java.awt.BorderLayout.CENTER);
 
-     jPanel2.add(jScrollPane1);
+		jPanel2.setLayout(new java.awt.GridLayout(1, 3));
 
-     close.setText("Fermer");
-     close.addActionListener(new java.awt.event.ActionListener() {
-     public void actionPerformed(java.awt.event.ActionEvent evt) {
-     closeActionPerformed(evt);
-     }
-     });
+		NewBut.setText("Nouveau");
+		NewBut.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				NewButActionPerformed(evt);
+			}
+		});
 
-     jPanel2.add(close);
+		jPanel2.add(NewBut);
 
-     getContentPane().add(jPanel2, java.awt.BorderLayout.SOUTH);
+		outPut.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+		jScrollPane1.setViewportView(outPut);
 
-     title.setBackground(new java.awt.Color(0, 102, 255));
-     title.setFont(new java.awt.Font("Century Gothic", 1, 14));
-     title.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-     title.setText(" Puissance 4 ");
-     getContentPane().add(title, java.awt.BorderLayout.NORTH);
+		jPanel2.add(jScrollPane1);
 
-     pack();
-     }
+		close.setText("Fermer");
+		close.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				closeActionPerformed(evt);
+			}
+		});
 
+		jPanel2.add(close);
 
-    private void playIActionPerformed(java.awt.event.ActionEvent evt, int i) {//GEN-FIRST:event_play7ActionPerformed
-	choice = i;
-	played = true;
-    }//GEN-LAST:event_play7ActionPerformed
+		getContentPane().add(jPanel2, java.awt.BorderLayout.SOUTH);
 
-     private void closeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_closeActionPerformed
-     // Add your handling code here:
-     System.exit(0);
-     }//GEN-LAST:event_closeActionPerformed
+		title.setBackground(new java.awt.Color(0, 102, 255));
+		title.setFont(new java.awt.Font("Century Gothic", 1, 14));
+		title.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+		title.setText(" Puissance 4 ");
+		getContentPane().add(title, java.awt.BorderLayout.NORTH);
 
-     private void NewButActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NewButActionPerformed
-     // Add your handling code here:
-  
-     }
-    // Exit the Application /
-     private void exitForm(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_exitForm
-     System.exit(0);
-     }
+		pack();
+	}
 
-    public void updateScreen(DataStructure my_grid) {
-     for(int i=0; i<6; i++)
-         for(int j=0; j<7; j++){
-             if(my_grid.getValue(i,j) == 0)
-		 playZone.getComponent(j+(7*i)).setBackground(new java.awt.Color(240,240,240));
-             if(my_grid.getValue(i,j) == 1)
-             playZone.getComponent(j+(7*i)).setBackground(new java.awt.Color(255,0,0));
-             if(my_grid.getValue(i,j) == 2)
-             playZone.getComponent(j+(7*i)).setBackground(new java.awt.Color(255,255,0));
-        }
-     }
+	private void playIActionPerformed(java.awt.event.ActionEvent evt, int i) {// GEN-FIRST:event_play7ActionPerformed
+		choice = i;
+		played = true;
+	}// GEN-LAST:event_play7ActionPerformed
 
-    public void game_ended(boolean winner){
-	if(!winner)
-	    outPut.setText("Joueur 1 a gagne !~");
-	else
-	    outPut.setText("Joueur 2 a gagne !~");
+	private void closeActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_closeActionPerformed
+		// Add your handling code here:
+		System.exit(0);
+	}// GEN-LAST:event_closeActionPerformed
 
-	//	play.setEnabled(false);
-    }
+	private void NewButActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_NewButActionPerformed
+		// Add your handling code here:
 
-    public void grey_button(int num){
-	switch(num)
-	    {
-	    case 0:
-		play.setEnabled(false);
-		break;
-	    case 1:
-		play2.setEnabled(false);
-		break;
-	    case 2:
-		play3.setEnabled(false);
-		break;
-	    case 3:
-		play4.setEnabled(false);
-		break;
-	    case 4:
-		play5.setEnabled(false);
-		break;
-	    case 5:
-		play6.setEnabled(false);
-		break;
-	    case 6:
-		play7.setEnabled(false);
-		break;
-	    }
-    }
+	}
+
+	// Exit the Application /
+	private void exitForm(java.awt.event.WindowEvent evt) {// GEN-FIRST:event_exitForm
+		System.exit(0);
+	}
+
+	public void updateScreen(DataStructure my_grid) {
+		for (int i = 0; i < 6; i++)
+			for (int j = 0; j < 7; j++) {
+				if (my_grid.getValue(i, j) == 0)
+					playZone.getComponent(j + (7 * i)).setBackground(
+							new java.awt.Color(240, 240, 240));
+				if (my_grid.getValue(i, j) == 1)
+					playZone.getComponent(j + (7 * i)).setBackground(
+							new java.awt.Color(255, 0, 0));
+				if (my_grid.getValue(i, j) == 2)
+					playZone.getComponent(j + (7 * i)).setBackground(
+							new java.awt.Color(255, 255, 0));
+			}
+	}
+
+	public void game_ended(boolean winner) {
+		if (!winner)
+			outPut.setText("Joueur 1 a gagne !~");
+		else
+			outPut.setText("Joueur 2 a gagne !~");
+
+		// play.setEnabled(false);
+	}
+
+	public void grey_button(int num) {
+		switch (num) {
+		case 0:
+			play.setEnabled(false);
+			break;
+		case 1:
+			play2.setEnabled(false);
+			break;
+		case 2:
+			play3.setEnabled(false);
+			break;
+		case 3:
+			play4.setEnabled(false);
+			break;
+		case 4:
+			play5.setEnabled(false);
+			break;
+		case 5:
+			play6.setEnabled(false);
+			break;
+		case 6:
+			play7.setEnabled(false);
+			break;
+		}
+	}
 
 }
--- src/GameEngine.java
+++ src/GameEngine.java
@@ -3,270 +3,289 @@ package src;
 import java.util.*;
 
 public class GameEngine {
-    
-    private DataStructure grid;
-    private boolean current_player;
-    private int currently_played;
-    private GUI app;
-    private int mode;
-    int counter;
-    
-    public GameEngine(int my_mode) {
-	this.grid = new DataStructure(6,7);
-     	this.current_player = false;
 
-        app = new GUI();
-	app.initGui();
-        app.setSize(500,550);
-        app.setLocation(100,100);
-        app.show();
+	private DataStructure grid;
+	private boolean current_player;
+	private int currently_played;
+	private GUI app;
+	private int mode;
+	int counter;
 
-	this.mode = my_mode;
-	counter = 0;
+	@SuppressWarnings("deprecation")
+	public GameEngine(int my_mode) {
+		this.grid = new DataStructure(6, 7);
+		this.current_player = false;
 
-	switch(mode){
-	case 0:
-	    {   start0(); // human vs human
-		break;}
-	case 1:
-	    {	    start1();
-		break;}
-	case 2:
-	    {	    start2();		
-		break;}
-	}
-    }
+		app = new GUI();
+		app.initGui();
+		app.setSize(500, 550);
+		app.setLocation(100, 100);
+		app.show();
 
-	public void start0(){
+		this.mode = my_mode;
+		counter = 0;
 
-	    while((!isComplete()) && (counter < 42) )
-		{
-		    while(!app.played);
-		    app.played = false;
-		    currently_played = app.choice;
-		    if(check_play(currently_played)){
-			update_grid();
-			app.updateScreen(grid);
-			current_player = !current_player;
-			grey_out();
-			counter++;
-		    }else
-			{
-			    System.out.print("AHHHHHHHHHHH!!!");
-			    System.out.println();
-			}
-			
+		switch (mode) {
+		case 0: {
+			start0(); // human vs human
+			break;
 		}
-	    
-	    app.game_ended(!current_player);
+		case 1: {
+			start1();
+			break;
+		}
+		case 2: {
+			start2();
+			break;
+		}
+		}
 	}
 
-    public void start1(){}
-    public void start2(){}
+	public void start0() {
+
+		while ((!isComplete()) && (counter < 42)) {
+			while (!app.played)
+				;
+			app.played = false;
+			currently_played = app.choice;
+			if (check_play(currently_played)) {
+				update_grid();
+				app.updateScreen(grid);
+				current_player = !current_player;
+				grey_out();
+				counter++;
+			} else {
+				System.out.print("");
+			}
+		}
+
+		app.game_ended(!current_player);
+	}
+
+	public void start1() {
+		while ((!isComplete()) && (counter < 42)) {
+			if (!current_player) { // human player
+				while (!app.played)
+					;
+				app.played = false;
+				currently_played = app.choice;
+			} else { // silly CPU
+				CPU cpu1 = new CPU(grid, 1);
+				currently_played = cpu1.play(app.choice);
+				if (currently_played == -1) {
+					// never happen but we have to considerate that possibility
+				}
+			}
+			if (check_play(currently_played)) {
+				update_grid();
+				app.updateScreen(grid);
+				current_player = !current_player;
+				grey_out();
+				counter++;
+			} else {
+				System.out.print("");
+			}
+		}
+
+	}
+
+	public void start2() {
+	}
 
 	private void update_grid() {
-	    int value = currently_played;
-	    int i = 0;
-	    boolean empty = true;
-	    
-	    while(i<6)
-		{
-		    if(grid.getValue(i,value) != 0)
-			{
-			    empty = false;
-			    break;
-			}
-		    else
-			i++;
+		int value = currently_played;
+		int i = 0;
+		boolean empty = true;
+
+		while (i < 6) {
+			if (grid.getValue(i, value) != 0) {
+				empty = false;
+				break;
+			} else
+				i++;
 		}
 
-	    if(empty)
-		{
-		    if(current_player == false)
-			grid.setValue(5,value,1);
-		    else
-			grid.setValue(5,value,2);
-		}
-	    else
-		{
-		    if(i != 0){
-			if(current_player == false)
-			    grid.setValue(i-1,value,1);
+		if (empty) {
+			if (current_player == false)
+				grid.setValue(5, value, 1);
 			else
-			    grid.setValue(i-1,value,2);
-		    }
+				grid.setValue(5, value, 2);
+		} else {
+			if (i != 0) {
+				if (current_player == false)
+					grid.setValue(i - 1, value, 1);
+				else
+					grid.setValue(i - 1, value, 2);
+			}
 		}
-	    
+
 	}
 
 	private boolean isComplete() {
-	    int color;
-	    int i,j;
-	    for(i = 0; i<6; i++)
-		{
-		    for(j = 0; j<7; j++)
-			{
-			    color = grid.getValue(i,j);
-			    if(color != 0){
-				if(check_line(i,j,color))
-				    return true;
-				if(check_col(i,j,color))
-				    return true;
-				if(check_diag(i,j,color))
-				    return true;
-			    }
+		int color;
+		int i, j;
+		for (i = 0; i < 6; i++) {
+			for (j = 0; j < 7; j++) {
+				color = grid.getValue(i, j);
+				if (color != 0) {
+					if (check_line(i, j, color))
+						return true;
+					if (check_col(i, j, color))
+						return true;
+					if (check_diag(i, j, color))
+						return true;
+				}
 			}
 		}
-	    return false;
-	}
-
-	private boolean check_line(int i,int j,int color) {
-	    int count_left = 0;
-	    int count_right = 0;
-
-	    int real_j = j;
-
-	    if(j>0){
-	    j= real_j-1;
-	    while((j>=0) && (j>(real_j-4)) && (grid.getValue(i,j) == color))
-		{
-		    count_left++;
-		    j--;
-		}
-	    }
-	    
-	    if(j<6){
-	    j= real_j+1;
-	    while((j<7) && (j<(real_j+4)) && (grid.getValue(i,j) == color))
-		{
-		    count_right++;
-		    j++;
-		}
-	    }	
-	    if((count_left + count_right) >=3)
-		return true;
-	    else
 		return false;
 	}
-	
-	private boolean check_col(int i,int j,int color) {
-	    int count_up = 0;
-	    int count_down = 0;
 
-	    int real_i = i;
+	private boolean check_line(int i, int j, int color) {
+		int count_left = 0;
+		int count_right = 0;
 
-	    if(i>0){
-	    i= real_i-1;
-	    while((i>=0) && (i>(real_i-4)) && (grid.getValue(i,j) == color))
-		{
-		    count_down++;
-		    i--;
+		int real_j = j;
+
+		if (j > 0) {
+			j = real_j - 1;
+			while ((j >= 0) && (j > (real_j - 4))
+					&& (grid.getValue(i, j) == color)) {
+				count_left++;
+				j--;
+			}
 		}
-	    }
-	    if(i<5){
-	    i= real_i+1;
-	    while((i<6) && (i<(real_i+4)) && (grid.getValue(i,j) == color))
-		{
-		    count_up++;
-		    i++;
-		}
-	    }
 
-	    if((count_up + count_down) >=3)
-		return true;
-	    else
+		if (j < 6) {
+			j = real_j + 1;
+			while ((j < 7) && (j < (real_j + 4))
+					&& (grid.getValue(i, j) == color)) {
+				count_right++;
+				j++;
+			}
+		}
+		if ((count_left + count_right) >= 3)
+			return true;
+		else
+			return false;
+	}
+
+	private boolean check_col(int i, int j, int color) {
+		int count_up = 0;
+		int count_down = 0;
+
+		int real_i = i;
+
+		if (i > 0) {
+			i = real_i - 1;
+			while ((i >= 0) && (i > (real_i - 4))
+					&& (grid.getValue(i, j) == color)) {
+				count_down++;
+				i--;
+			}
+		}
+		if (i < 5) {
+			i = real_i + 1;
+			while ((i < 6) && (i < (real_i + 4))
+					&& (grid.getValue(i, j) == color)) {
+				count_up++;
+				i++;
+			}
+		}
+
+		if ((count_up + count_down) >= 3)
+			return true;
+		else
+			return false;
+
+	}
+
+	private boolean check_diag(int i, int j, int color) {
+
+		int count1 = 0;
+		int count2 = 0;
+
+		int real_i = i;
+		int real_j = j;
+
+		if ((i > 0) && (j > 0)) {
+			i = real_i - 1;
+			j = real_j - 1;
+			while ((i >= 0) && (i > (real_i - 4)) && (j >= 0)
+					&& (j > (real_j - 4)) && (grid.getValue(i, j) == color)) {
+				count1++;
+				i--;
+				j--;
+			}
+		}
+
+		if ((i < 5) && (j < 6)) {
+			i = real_i + 1;
+			j = real_j + 1;
+			while ((i < 6) && (i < (real_i + 4)) && (j < 7)
+					&& (j < (real_j + 4)) && (grid.getValue(i, j) == color)) {
+				count2++;
+				i++;
+				j++;
+			}
+		}
+
+		if ((count1 + count2) >= 3)
+			return true;
+
+		count1 = 0;
+		count2 = 0;
+
+		if ((i > 0) && (j < 6)) {
+			i = real_i - 1;
+			j = real_j + 1;
+			while ((i >= 0) && (i > (real_i - 4)) && (j < 7)
+					&& (j < (real_j + 4)) && (grid.getValue(i, j) == color)) {
+				count1++;
+				i--;
+				j++;
+			}
+		}
+
+		if ((i < 5) && (j > 0)) {
+			i = real_i + 1;
+			j = real_j - 1;
+			while ((i < 6) && (i < (real_i + 4)) && (j >= 0)
+					&& (j >= (real_j - 4)) && (grid.getValue(i, j) == color)) {
+				count2++;
+				i++;
+				j--;
+			}
+		}
+
+		if ((count1 + count2) >= 3)
+			return true;
+
 		return false;
-
-	}
-	
-	private boolean check_diag(int i,int j,int color) {
-	
-	    int count1 = 0;
-	    int count2 = 0;
-
-	    int real_i = i;
-	    int real_j = j;
-
-	    if((i>0) && (j>0)){
-	    i= real_i-1;
-	    j= real_j-1;
-	    while((i>=0) && (i>(real_i-4)) && (j>=0) && (j>(real_j-4)) && (grid.getValue(i,j) == color))
-		{
-		    count1++;
-		    i--;
-		    j--;
-		}
-	    }
-
-	    if((i<5) && (j<6)){
-	    i= real_i+1;
-	    j= real_j+1;
-	    while((i<6) && (i<(real_i+4)) && (j<7) && (j<(real_j+4)) && (grid.getValue(i,j) == color))
-		{
-		    count2++;
-		    i++;
-		    j++;
-		}
-	    }
-
-	    if((count1 + count2) >=3)
-		return true;
-
-
-	    count1 = 0;
-	    count2 = 0;
-
-	    if((i>0) && (j<6)){
-	    i= real_i-1;
-	    j= real_j+1;
-	    while((i>=0) && (i>(real_i-4)) && (j<7) && (j<(real_j+4)) && (grid.getValue(i,j) == color))
-		{
-		    count1++;
-		    i--;
-		    j++;
-		}
-	    }
-
-	    if((i<5) && (j>0)){
-	    i= real_i+1;
-	    j= real_j-1;
-	    while((i<6) && (i<(real_i+4)) && (j>=0) && (j>=(real_j-4)) && (grid.getValue(i,j) == color))
-		{
-		    count2++;
-		    i++;
-		    j--;
-		}
-	    }
-
-	    if((count1 + count2) >=3)
-		return true;
-
-	    return false;
 	}
 
-    private boolean check_play(int play){
-	if(grid.getValue(0,play) == 0)
-	    return true;
-	return false;
-    }
+	private boolean check_play(int play) {
+		if (grid.getValue(0, play) == 0)
+			return true;
+		return false;
+	}
 
-    private ArrayList<Integer> playable(){
-	int i;
-	ArrayList<Integer> cols = new ArrayList<Integer>();
-	for(i = 0; i<7; i++)
-	    {
-		if(check_play(i))
-		    cols.add(i);
-	    }
-	return cols;
-    }
-    
-    private void grey_out(){
-	int i;
-	for(i = 0; i<7; i++)
-	    if(grid.getValue(0,i)!=0)
-		app.grey_button(i);
+	@SuppressWarnings("unused")
+	private ArrayList<Integer> playable() {
+		int i;
+		ArrayList<Integer> cols = new ArrayList<Integer>();
+		for (i = 0; i < 7; i++) {
+			if (check_play(i))
+				cols.add(i);
+		}
+		return cols;
+	}
 
-    }
-    
+	private void grey_out() {
+		int i;
+		for (i = 0; i < 7; i++)
+			if (grid.getValue(0, i) != 0)
+				app.grey_button(i);
+
+	}
+
 }
