684f13c3ebfb161e07b3760329b554638aff092e
dcransac <dcransac@c935e6ca-b7f9-11dd-b612-3927438af0ba>
2009-01-23 16:30:25 +0000

    23-01-09 17-30

--- trunk/src/DataStructure.java
+++ trunk/src/DataStructure.java
@@ -1,41 +1,48 @@
 package src;
 
 public class DataStructure {
-	private int[][] matrix;
-	int height;
-	int width;
-	
-	public
-	DataStructure(int height, int width) {
-		this.height=height;
-		this.width=width;
-		this.matrix = new int[height][width];
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				matrix[i][j] = 0;
-			}
-		}
+    private int[][] matrix;
+    int height;
+    int width;
+    
+    public DataStructure(int height, int width) {
+	this.height=height;
+	this.width=width;
+	this.matrix = new int[height][width];
+	for (int i = 0; i < height; i++) {
+	    for (int j = 0; j < width; j++) {
+		matrix[i][j] = 0;
+	    }
 	}
-	
-	public int[][] getMatrix(){
-		return matrix;
+    }
+    
+    public int[][] getMatrix(){
+	return matrix;
+    }
+        
+    public void print(){
+	for(int i=0;i<height; i++){
+	    System.out.print("Ligne : " + i + " | ");
+                        for(int j=0;j<width; j++){
+			    System.out.print(matrix[i][j] + " ");
+                        }
+                        System.out.println();
 	}
-	
-	public void print(){
-		for(int i=0;i<height; i++){
-			System.out.print("Ligne : " + i + " | ");
-			for(int j=0;j<width; j++){
-				System.out.print(matrix[i][j] + " ");
-			}
-			System.out.println();
-		}
+    }
+    
+    public boolean setValue(int i, int j, int color){
+	if(matrix[i][j]==0){
+	    matrix[i][j]=color;
+	    return true;
+	}else{
+	    System.out.print("[DataStructure] : Error in setValue");
+	    System.out.println();
 	}
-	
-	public boolean setMatrix(int i, int j, int color){
-		if(matrix[i][j]==0){
-			matrix[i][j]=color;
-			return true;
-		}
-		return false;
-	}
+	return false;
+    }
+    
+    public int getValue(int i, int j){
+	return matrix[i][j];
+    }
+    
 }
