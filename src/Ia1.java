f3944cf5d8c6b0c1d436f6e10884e2c06c927820
binogure <binogure@c935e6ca-b7f9-11dd-b612-3927438af0ba>
2009-01-24 15:36:18 +0000

    Premier JUnit

--- src/Ia1.java
+++ src/Ia1.java
@@ -5,10 +5,19 @@ public class Ia1 implements Cpu{
 	private int mode;
 	private DataStructure cpugrid;
 	private int last_played;
+	private int[] playable;
+	/* 0 Playable */
+	/* 1 Human wins */
+	/* 2 Break strategy */
+	
+	private int height;
+	private int width;
 	
 	public void initialize(DataStructure grid, int difficulty) {
 		cpugrid = grid;
 		mode = difficulty;
+		height = cpugrid.getHeight();
+		width = cpugrid.getWidth();
 	};
 	
 	public int play(int played) {
@@ -20,69 +29,69 @@ public class Ia1 implements Cpu{
 	
 	public int easy_cpu() {
 		int result = -1;
-		for (int i = 1; i < 4; ++i) { // human attempt a column 3
-			if (cpugrid.getValue(6 - i, last_played) == cpugrid.getValue(
-					6 - (i + 1), last_played)
-					&& cpugrid.getValue(6 - i, last_played) == cpugrid
-							.getValue(6 - (i + 2), last_played)
-					&& cpugrid.getValue(6 - (i + 3), last_played) == 0
+		for (int i = 1; i < width-3; ++i) { // human attempt a column 3
+			if (cpugrid.getValue(height - i, last_played) == cpugrid.getValue(
+					height - (i + 1), last_played)
+					&& cpugrid.getValue(height - i, last_played) == cpugrid
+							.getValue(height - (i + 2), last_played)
+					&& cpugrid.getValue(height - (i + 3), last_played) == 0
 					&& cpugrid.getValue(6 - i, last_played) != 0)
 				result = last_played;
 		}
-		if (last_played < 6 && last_played > 1 && result == -1) // human attempt
+		if (last_played < height && last_played > 1 && result == -1) // human attempt
 																// a line 3
-			for (int i = 1; i < 6; ++i) {
-				if (cpugrid.getValue(6 - i, last_played - 2) == cpugrid
-						.getValue(6 - i, last_played - 1)
-						&& cpugrid.getValue(6 - i, last_played - 1) == cpugrid
-								.getValue(6 - i, last_played)
-						&& cpugrid.getValue(6 - i, last_played + 1) == 0
-						&& cpugrid.getValue(6 - i, last_played) != 0)
+			for (int i = 1; i < height; ++i) {
+				if (cpugrid.getValue(height - i, last_played - 2) == cpugrid
+						.getValue(height - i, last_played - 1)
+						&& cpugrid.getValue(height - i, last_played - 1) == cpugrid
+								.getValue(height - i, last_played)
+						&& cpugrid.getValue(height - i, last_played + 1) == 0
+						&& cpugrid.getValue(height - i, last_played) != 0)
 					result = last_played + 1;
 			}
-		if (last_played > 0 && last_played < 4 && result == -1) // human attempt
+		if (last_played > 0 && last_played < width-3 && result == -1) // human attempt
 																// a line 3
-			for (int i = 1; i < 6; ++i) {
-				if (cpugrid.getValue(6 - i, last_played + 2) == cpugrid
-						.getValue(6 - i, last_played + 1)
-						&& cpugrid.getValue(6 - i, last_played + 1) == cpugrid
-								.getValue(6 - i, last_played)
-						&& cpugrid.getValue(6 - i, last_played - 1) == 0
-						&& cpugrid.getValue(6 - i, last_played) != 0)
+			for (int i = 1; i < height; ++i) {
+				if (cpugrid.getValue(height - i, last_played + 2) == cpugrid
+						.getValue(height - i, last_played + 1)
+						&& cpugrid.getValue(height - i, last_played + 1) == cpugrid
+								.getValue(height - i, last_played)
+						&& cpugrid.getValue(height - i, last_played - 1) == 0
+						&& cpugrid.getValue(height - i, last_played) != 0)
 					result = last_played - 1;
 			}
 
 		if (result == -1) {
-			for (int i = 1; i < 4; ++i) { // human attempt a column 2
-				if (cpugrid.getValue(6 - i, last_played) == cpugrid.getValue(
-						6 - (i + 1), last_played)
-						&& cpugrid.getValue(6 - (i + 2), last_played) == 0
-						&& cpugrid.getValue(6 - i, last_played) != 0)
+			for (int i = 1; i < width-3; ++i) { // human attempt a column 2
+				if (cpugrid.getValue(height - i, last_played) == cpugrid.getValue(
+						height - (i + 1), last_played)
+						&& cpugrid.getValue(height - (i + 2), last_played) == 0
+						&& cpugrid.getValue(height - i, last_played) != 0)
 					result = last_played;
 			}
 		}
 		if (result == -1) {
-			if (last_played < 6 && last_played > 1) // human attempt a line 2
-				for (int i = 1; i < 6; ++i) {
-					if (cpugrid.getValue(6 - i, last_played - 1) == cpugrid
-							.getValue(6 - i, last_played)
-							&& cpugrid.getValue(6 - i, last_played + 1) == 0
-							&& cpugrid.getValue(6 - i, last_played) != 0)
+			if (last_played < height && last_played > 1) // human attempt a line 2
+				for (int i = 1; i < height; ++i) {
+					if (cpugrid.getValue(height - i, last_played - 1) == cpugrid
+							.getValue(height - i, last_played)
+							&& cpugrid.getValue(height - i, last_played + 1) == 0
+							&& cpugrid.getValue(height - i, last_played) != 0)
 						result = last_played + 1;
 				}
 		}
 		if (result == -1) {
-			if (last_played > 0 && last_played < 6) // human attempt a line 2
-				for (int i = 1; i < 6; ++i) {
-					if (cpugrid.getValue(6 - i, last_played + 1) == cpugrid
-							.getValue(6 - i, last_played)
-							&& cpugrid.getValue(6 - i, last_played - 1) == 0
-							&& cpugrid.getValue(6 - i, last_played) != 0)
+			if (last_played > 0 && last_played < height) // human attempt a line 2
+				for (int i = 1; i < height; ++i) {
+					if (cpugrid.getValue(height - i, last_played + 1) == cpugrid
+							.getValue(height - i, last_played)
+							&& cpugrid.getValue(height - i, last_played - 1) == 0
+							&& cpugrid.getValue(height - i, last_played) != 0)
 						result = last_played - 1;
 				}
 		}
 		if (result == -1) {
-			for (int i = 0; i < 7; ++i)
+			for (int i = 0; i < width; ++i)
 				if (cpugrid.getValue(0, i) == 0)
 					result = i;
 		}
@@ -91,6 +100,8 @@ public class Ia1 implements Cpu{
 
 	public int perfect_cpu() {
 		int result = -1;
+		playable = new int[cpugrid.getWidth()];
+		
 		
 		return result;
 	}
--- test/DataStructureTest.java
+++ test/DataStructureTest.java
@@ -10,6 +10,20 @@ public class DataStructureTest {
 	@Test
 	public void testDataStructure() {
 		matrix = new DataStructure(6,7);
-		matrix.print();
+		String result = "Ligne : 0 | 0 0 0 0 0 0 0\nLigne : 1 | 0 0 0 0 0 0 0\nLigne : 2 | 0 0 0 0 0 0 0\nLigne : 3 | 0 0 0 0 0 0 0\nLigne : 4 | 0 0 0 0 0 0 0\nLigne : 5 | 0 0 0 0 0 0 0\n";
+		String resulttmp = print();
+		assert(result.equals(resulttmp));
+	}
+	
+	public String print() {
+		String result = "";
+		for (int i = 0; i < matrix.getHeight(); i++) {
+			result +="Ligne : " + i + " | ";
+			for (int j = 0; j < matrix.getWidth(); j++) {
+				result +=matrix.getValue(i,j) + " ";
+			}
+			result += "\n";
+		}
+		return result;
 	}
 }
