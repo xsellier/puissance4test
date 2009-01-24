b778e1b0a7c596a4e1df19283c5a8f101a252d68
edouardewans <edouardewans@c935e6ca-b7f9-11dd-b612-3927438af0ba>
2009-01-24 18:55:37 +0000

    Junit standards sur la classe DataStructure à compléter

---
+++ trunk/test/DataTest.java
@@ -0,0 +1,47 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import src.*;
+
+public class DataTest {
+
+	@Test
+	public void testDataStructure() {
+		DataStructure data = new DataStructure(8,8);
+		assertEquals(8,data.getHeight());
+		assertEquals(8,data.getWidth());
+	}
+
+	@Test
+	public void testGetHeight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWidth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMatrix() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetValue() {
+		fail("Not yet implemented");
+	}
+
+}
