579acc2e9318e22c073761004816365807f16ffe
edouardewans <edouardewans@c935e6ca-b7f9-11dd-b612-3927438af0ba>
2009-01-27 16:34:01 +0000

    GameEngineTest squelette

---
+++ test/GameEngineTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameEngineTest {
+
+	@Test
+	public void testGameEngine() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClose() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReset_grid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheck_grid() {
+		fail("Not yet implemented");
+	}
+
+}
