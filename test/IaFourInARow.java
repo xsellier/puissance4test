efb56d6b178d78c1cf5ea930d1717b64bf6372b8
Binogure <Binogure@c935e6ca-b7f9-11dd-b612-3927438af0ba>
2009-01-28 22:44:46 +0000

    Squelette pour les JUnit de IAForInARow

---
+++ test/IaFourInARow.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IaFourInARow {
+
+	@Test
+	public void testInitialize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlay() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEasyCpu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPerfectCpu() {
+		fail("Not yet implemented");
+	}
+
+}
